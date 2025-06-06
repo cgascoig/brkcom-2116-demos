2c83b44a4ee46d4fd53f657c965cb7c02d1ed03e
Chris Gascoigne <cgascoig@cisco.com>
2025-06-06 11:59:16 +1000

    add variables

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "intersight_api_keyid" {
+  type = string
+}
+
+variable "intersight_secretkey" {
+  type = string
+}
+
+variable "intersight_endpoint" {
+    type = string
+    default = "https://www.intersight.com"
+}
